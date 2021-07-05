e091331108351d28de3ed9d03ff3891d5f2a09ec
Wspsxing <biluohc@qq.com>
2021-07-05 14:21:18 +0800

    use build.rollupOptions to skip docs/index.html

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 
+const { resolve } = require('path')
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "", // use relative directory
@@ -11,6 +13,11 @@ export default defineConfig({
     outDir: "dist",
     assetsDir: "assets",
     sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: resolve('./', 'index.html'),
+      }
+    }
   },
   plugins: [vue(), vueJsx()],
 });
